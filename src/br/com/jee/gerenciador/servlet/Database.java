19e0c55ec2a543e1b989493259fcaa143c75469a
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-04 20:05:21 -0300

    Criado um bloco estático para manter empresas cadastradas e mostrá-las a cada execução do projeto

--- src/br/com/jee/gerenciador/servlet/Database.java
+++ src/br/com/jee/gerenciador/servlet/Database.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Database 
 {
 	private static List<Company> lista = new ArrayList<>();
+	
+	static 
+	{
+		Company empresa1 = new Company();
+		empresa1.setNome("Tracta");
+		Company empresa2 = new Company();
+		empresa2.setNome("Vizzela");
+		lista.add(empresa1);
+		lista.add(empresa2);
+	}
 
 	public void adiciona(Company empresa) 
 	{	
