70215f87657a21fcbb1dc6c65c705eb7e5dae288
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-04 18:57:13 -0300

    Alteração do método service para doPost

--- src/br/com/jee/gerenciador/servlet/NewCompany.java
+++ src/br/com/jee/gerenciador/servlet/NewCompany.java
@@ -14,7 +14,7 @@ public class NewCompany extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		System.out.println("Cadastrando nova empresa");
 		
