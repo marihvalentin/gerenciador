340502f6164a7824e0450e144a2c0416a45806b3
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-04 17:39:50 -0300

    Subindo projeto Java EE para o git/github com estrutura criada. Desenvolvimento via Eclipse, o projeto já contém seu primeiro servlet (HelloWorld.java) no pack servlet e uma página teste html, no diretório WebContent

---
+++ src/br/com/jee/gerenciador/servlet/HelloWorld.java
@@ -0,0 +1,30 @@
+package br.com.jee.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//hello
+@WebServlet(urlPatterns = "/hello")
+public class HelloWorld extends HttpServlet
+{
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException
+  {
+	  PrintWriter out = resp.getWriter();
+	  out.println("<html>");
+	  out.println("<body>");
+	  out.println("<h1>");
+	  out.println("Congrats! You created the first Servlet.");
+	  out.println("</h1>");
+	  out.println("</body>");
+	  out.println("</html>");
+	  
+	  System.out.println("o servlet foi chamado");
+  }
+}
