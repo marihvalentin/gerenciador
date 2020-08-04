40a12183c0c9ab535ce919e06f8b1383113a6684
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-04 18:04:51 -0300

    Criado novo servlet newCompany que testa o cadastro de empresas. Por ele, foram realizados o envio de parâmetro na requisição por meio da requisição HTTP, a leitura do parâmetro e depois a concatenação com a resposta exibida no navegador

---
+++ src/br/com/jee/gerenciador/servlet/NewCompany.java
@@ -0,0 +1,33 @@
+package br.com.jee.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/newCompany")
+public class NewCompany extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		System.out.println("Cadastrando nova empresa");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h3>");
+		out.println("Company " + nomeEmpresa + " successfully registered");
+		out.println("</h3>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
