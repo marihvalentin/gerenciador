e05f6c0b1999be6cc8da63cc31a5c26a716b2c04
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-04 19:41:21 -0300

    Criada a classe Company para atribuir os dados do cadastro de uma empresa, como id e nome e gerados seus getters e setters

---
+++ src/br/com/jee/gerenciador/servlet/Company.java
@@ -0,0 +1,29 @@
+package br.com.jee.gerenciador.servlet;
+
+public class Company 
+{
+	private Integer id;
+	private String nome;
+	
+	public Integer getId() 
+	{
+		return id;
+	}
+	
+	public void setId(Integer id) 
+	{
+		this.id = id;
+	}
+	
+	public String getNome() 
+	{
+		return nome;
+	}
+	
+	public void setNome(String nome) 
+	{
+		this.nome = nome;
+	}
+	
+	
+}
