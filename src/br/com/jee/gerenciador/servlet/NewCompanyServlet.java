c723cf592c2c26338dee21efad06ec8052089f03
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-05 13:53:06 -0300

    Alteração no NewCompanyServlet que chama o arquivo JSP, responsável por mostrar a mensagem de empresa cadastrada e envia as informações inseridas para mostrar na mensagem do JSP

--- src/br/com/jee/gerenciador/servlet/NewCompanyServlet.java
+++ src/br/com/jee/gerenciador/servlet/NewCompanyServlet.java
@@ -2,7 +2,7 @@ package br.com.jee.gerenciador.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,14 +26,10 @@ public class NewCompanyServlet extends HttpServlet
 		Database banco = new Database();
 		banco.adiciona(empresa);
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<h3>");
-		out.println("Company " + nomeEmpresa + " successfully registered");
-		out.println("</h3>");
-		out.println("</body>");
-		out.println("</html>");
+		//chamar o JSP
+		RequestDispatcher rd = request.getRequestDispatcher("/newCompanyCreated.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
 	}
 
 }
